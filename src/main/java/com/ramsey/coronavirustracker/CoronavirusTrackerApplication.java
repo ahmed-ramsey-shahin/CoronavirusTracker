d6bef18f9b68f509b8e14ad70da60fddbe045f4c
Ahmed Ramsey <ahmed.ramsey.shahin@yandex.com>
2021-09-18 00:58:28 +0200

    Initial commit

---
+++ src/main/java/com/ramsey/coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,15 @@
+package com.ramsey.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusTrackerApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+		
+	}
+	
+}
